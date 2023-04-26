2724ab76079c7580935ee76098eb29ed556c7367
anton2920 <anton2920@gmail.com>
2023-04-26 17:16:30 +0300

    acme/acmego: added support for .c/.h formatting with cb(1)

--- acme/acmego/main_p9p.go
+++ acme/acmego/main_p9p.go
@@ -4,6 +4,11 @@ package main
 
 import "os/exec"
 
+// Non-Go formatters (only loaded with -f option).
+var otherFormatters = map[string][]string{
+	".rs": []string{"rustfmt", "--emit", "stdout"},
+}
+
 func ExecDiff(name, tmp string) *exec.Cmd {
 	return exec.Command("9", "diff", name, tmp)
 }
