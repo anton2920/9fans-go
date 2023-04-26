1334c0de6aa08fd133464cb47174f690eacf8706
anton2920 <anton2920@gmail.com>
2023-04-26 16:57:01 +0300

    all: fixed build on *nix

--- plan9/client/fid_plan9.go
+++ plan9/client/fid_plan9.go
@@ -1,7 +1,20 @@
+//go:build plan9
+
 package client
 
-import "os"
+import (
+	"os"
+	"syscall"
+)
 
 type Fid struct {
 	*os.File
+}
+
+func (fid *Fid) Write(b []byte) (n int, err error) {
+	/* NOTE(anton2920): on Plan 9 (*os.File).Write doesn't allow zero-byte writes.
+	 * syscall.Write, on the other hand, doesn't complain about that,
+	 * so we use that, because we need it.
+	 */
+	return syscall.Write(int(fid.Fd()), b)
 }
